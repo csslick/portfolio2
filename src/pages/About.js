30a626dcf0d462990183bccc808c717a3042bdec
JuHwan <juhwan203@kakao.com>
2023-01-31 14:38:52 +0900

    skill icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,19 @@ export default function About() {
   return (
     <>
       <Nav />
-      <div>About</div>
+      <div className="container title">
+        <h1>About</h1>
+        <p>내 소개...</p>
+      </div>
+      <div 
+        className="container" 
+        style={{textAlign: 'center', background: '#666'}}>
+        <h2>직무능력(Skills)</h2>
+        <img src="https://skillicons.dev/icons?i=html,css,js,jquery,nodejs,vscode,git,github,sqlite,bootstrap&perline=3&theme=light" />
+        <hr />
+        <img src="https://skillicons.dev/icons?i=photoshop,figma&theme=light" />
+      </div>
+
     </>
   )
 }
